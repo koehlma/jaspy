0e3fa0b49848c4d1bde88dbbbcf197a08a745195
Maximilian Köhl <mail@koehlma.de>
2016-03-04 00:55:21 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016, Maximilian Köhl <mail@koehlma.de>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+setup(name='jaspy',
+      version='0.0.1dev',
+      author='Maximilian Köhl',
+      author_email='mail@koehlma.de',
+      url='https://www.koehlma.de/jaspy',
+      license='LGPLv3',
+      py_modules=['jaspy'],
+      classifiers=[])
