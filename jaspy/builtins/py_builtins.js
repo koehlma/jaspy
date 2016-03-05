cc9c6e63249e32ca892f7c0ac539babc2b873aec
Maximilian Köhl <mail@koehlma.de>
2016-03-05 06:25:40 +0100

    add license headers

--- jaspy/builtins/py_builtins.js
+++ jaspy/builtins/py_builtins.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016, Maximilian Koehl <mail@koehlma.de>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var build_class = new_native(function (func, name, bases, metaclass, keywords, state, frame) {
     var possible_meta_classes, index, good, bases;
     if (!(func.cls == py_function)) {
