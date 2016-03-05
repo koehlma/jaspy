8b00f9c47fa4eb598373dbb1683e8c6492ca0ecd
Maximilian Köhl <mail@koehlma.de>
2016-03-05 07:00:21 +0100

    add missing builtins/__init__.js

---
+++ jaspy/builtins/__init__.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016, Maximilian Koehl <mail@koehlma.de>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// #include "builtins/py_object.js"
+// #include "builtins/py_type.js"
+// #include "builtins/py_dict.js"
+// #include "builtins/py_int.js"
+// #include "builtins/py_bool.js"
+// #include "builtins/py_float.js"
+// #include "builtins/py_str.js"
+// #include "builtins/py_function.js"
+// #include "builtins/py_method.js"
+// #include "builtins/py_property.js"
+// #include "builtins/py_module.js"
+// #include "builtins/py_none.js"
+// #include "builtins/py_exception.js"
+
+// #include "builtins/py_builtins.js"
