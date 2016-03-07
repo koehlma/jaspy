39056966b25f34a26b1ca7806a0d7587a0818229
Maximilian Köhl <mail@koehlma.de>
2016-03-07 21:42:45 +0100

    add speed test code

--- example/speed.py
+++ example/speed.py
@@ -1,4 +1,60 @@
-x = 0
-while x < 5000:
-    x += 1
-print(x)
+import time
+import dom
+
+
+def run():
+    start = time.time()
+    x = 0
+    while x < 5000:
+        x += 1
+    stop = time.time()
+    return stop - start
+
+
+def run1():
+    start = time.time()
+    x = 0
+    while x < 5000:
+        dom.Element('div')
+        x += 1
+    stop = time.time()
+    return stop - start
+
+a = 0
+total = 0
+while a < 10:
+    a += 1
+    total += run()
+print(total / 10)
+
+c = '''
+import js
+import time
+
+document = js.globals['document']
+
+def run():
+    start = time.time()
+
+    x = 0
+    while x < 5000:
+        document.createElement('div')
+        x += 1
+    stop = time.time()
+    return stop - start
+
+def run():
+    start = time.time()
+    x = 0
+    while x < 5000:
+        x += 1
+    stop = time.time()
+    return stop - start
+
+a = 0
+total = 0
+while a < 10:
+    a += 1
+    total += run()
+print(total / 10)
+'''
