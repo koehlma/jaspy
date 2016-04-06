b6b71362a1e3844aabdc904b1e47ee0c47d875cf
Maximilian Köhl <mail@koehlma.de>
2016-04-06 22:45:54 +0200

    doc fix broken jaspy import

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,12 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import sys
+
+__dir__ = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(__dir__, '..', '..'))
+
 from jaspy import metadata
 
 extensions = [
