250fd55b0eb0e8b5c4ca752dd4058be438a24d69
Maximilian Köhl <mail@koehlma.de>
2016-04-07 00:27:22 +0200

    [karma] pre-process jaspy.js with coverage

--- .karma.js
+++ .karma.js
@@ -1,20 +1,20 @@
-module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
-    files: [
-      'build/jaspy.js',
-      'spec/*.js'
-    ],
-    browsers: ['PhantomJS'],
-    singleRun: true,
-    reporters: ['progress', 'coverage'],
-    preprocessors: { '*.js': ['coverage'] },
-    coverageReporter: {
-	    type : 'lcov',
-	    dir : 'coverage/',
-	    subdir: '.'
-    },
-    browserNoActivityTimeout: 60000
-  });
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'build/jaspy.js',
+            'spec/*.js'
+        ],
+        browsers: ['PhantomJS'],
+        singleRun: true,
+        reporters: ['progress', 'coverage'],
+        preprocessors: {'build/jaspy.js': ['coverage']},
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage/',
+            subdir: '.'
+        },
+        browserNoActivityTimeout: 60000
+    });
 };
