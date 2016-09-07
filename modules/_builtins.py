8b44d82ac8756b1ed21141c73968b0602fd89cf5
Maximilian Köhl <mail@koehlma.de>
2016-09-07 15:10:57 +0200

    implement more builtins

--- modules/_builtins.py
+++ modules/_builtins.py
@@ -13,18 +13,14 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
+"""
+Implementation of non-trivial Python builtin functions.
+
+https://docs.python.org/3/library/functions.html
+"""
+
 
 NULL = object()
-
-
-def next(iterator, default=NULL):
-    if default is NULL:
-        return iterator.__next__()
-    else:
-        try:
-            return iterator.__next__()
-        except StopIteration:
-            return default
 
 
 def all(iterable):
@@ -39,6 +35,47 @@ def any(iterable):
         if element:
             return True
     return False
+
+
+def next(iterator, default=NULL):
+    if default is NULL:
+        return iterator.__next__()
+    else:
+        try:
+            return iterator.__next__()
+        except StopIteration:
+            return default
+
+
+def _counter(start=0):
+    while True:
+        yield start
+        start += 1
+
+
+def enumerate(iterable, start=0):
+    return zip(_counter(start), iterable)
+
+
+def filter(function, iterable):
+    if function is None:
+        return (item for item in iterable if item)
+    else:
+        return (item for item in iterable if function(item))
+
+
+def map(function, *iterables):
+    return (function(*args) for args in zip(*iterables))
+
+
+def pow(x, y, z=None):
+    return x ** y if z is None else (x ** y) % z
+
+
+def sum(iterable, start=0):
+    for item in iterable:
+        start += item
+    return start
 
 
 def zip(*iterables):
